87b84c6eea9959b38a3b3afe739e89cb6398f292
Jeroen Doggen <jeroendoggen@gmail.com>
2015-06-23 11:39:21 +0200

    Added experiment skeleton files

---
+++ Experiments/button_press.ino
@@ -0,0 +1,34 @@
+// Arduino Interactive: button t
+// Copyright 2015 Jeroen Doggen, Christian Munk, Tom Peeters
+
+int delayTime = 10000;                            // 10 seconds
+int buttonState = 0;
+
+void setup()
+{
+     //TODO                                      // configure the input pins
+     //TODO                                      // configure the output pins
+     Serial.begin(9600);                         // use the serial port
+}
+
+void loop()
+{
+  if(readSensors() == true)
+  {
+    takePicture();
+  }
+  delay(delayTime);                              // delay between pictures
+}
+
+int readSensors()
+{
+//PSEUDO code:
+// read the sensors here
+
+  return(buttonState);
+}
+
+void takePicture()
+{
+// do stuff here
+}
---
+++ Experiments/stop_motion.ino
@@ -0,0 +1,22 @@
+// Arduino Interactive: stop-motion example
+// Copyright 2015 Jeroen Doggen, Christian Munk, Tom Peeters
+
+int delayTime = 10000;                            // 10 seconds
+
+void setup()
+{
+     //TODO                                      // configure the input pins
+     //TODO                                      // configure the output pins
+     Serial.begin(9600);                         // use the serial port
+}
+
+void loop()
+{
+  takePicture();
+  delay(delayTime);                              // delay between pictures
+}
+
+void takePicture()
+{
+// do stuff here
+}
